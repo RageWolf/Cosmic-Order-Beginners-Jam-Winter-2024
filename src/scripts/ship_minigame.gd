a2a114eec29572e1cb33b24601791069106e5785
RageWolf <Dr4gonDestroyer@gmail.com>
2024-12-29 18:56:08 +0100

    changes for merging

--- src/scripts/ship_minigame.gd
+++ src/scripts/ship_minigame.gd
@@ -1,7 +1,7 @@
 class_name ShipMinigame extends Node2D
 
 @export var starting_pos : Vector2 = Vector2(100,240)
-var Player : PlayerShip = preload("res://src/player_ship.tscn").instantiate()
+var Player : PlayerShip = preload("res://src/player_ship_2.tscn").instantiate()
 
 # Base class for all minigames that are played on a
 # sideview with the spaceship, currently only used for
